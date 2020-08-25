d5bcd5719e9038f63e652d85b421b697ddae87b8
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-24 22:17:37 -0300

    feat(base-entity.ts): create the base entity for typeorm

---
+++ src/common/base-entity.ts
@@ -0,0 +1,25 @@
+import { IsOptional } from 'class-validator';
+
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+import { CrudValidationGroups } from '@nestjsx/crud';
+const { CREATE, UPDATE } = CrudValidationGroups;
+
+export class BaseEntity {
+
+    @IsOptional({ groups: [CREATE, UPDATE]})
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @IsOptional({ groups: [CREATE, UPDATE] })
+    @CreateDateColumn()
+    createAt: Date
+
+    @IsOptional({ groups: [CREATE, UPDATE] })
+    @UpdateDateColumn()
+    updateAt: Date
+
+    @IsOptional({ groups: [CREATE, UPDATE] })
+    @Column({ default: true })
+    isActive: boolean
+}
