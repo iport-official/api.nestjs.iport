53d892aac682e1eeca00d9c7cdb3faff6bf21823
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-27 08:08:56 -0300

    feat(migrations): add CreatePeople migration and the CreateCompanies migrations

--- src/typeorm/migrations/1598445111936-CreatePeople.ts
+++ src/typeorm/migrations/1598445111936-CreatePeople.ts
@@ -3,9 +3,11 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreatePeople1598445111936 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+
     }
 
 }
