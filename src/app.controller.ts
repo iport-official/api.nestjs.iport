e1cc545ae0db9866818f10b0f137522fd7e7ec00
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-31 22:13:36 -0300

    feat(app.controller.ts): add UseGuard with local-strategy in app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Request, UseGuards, Post } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    @UseGuards(AuthGuard('local'))
+    @Post('auth/login')
+    async login(@Request() req) {
+        return req.user;
+    }
 }
