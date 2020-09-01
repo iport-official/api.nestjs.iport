7ef08b4cfd5002cffa4030ffd08512b17b25008f
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-01 18:26:41 -0300

    feat(auth): modularizing the jwt strategy

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { UserService } from './user.service';
 import UserInterface from './interfaces/UserInterface';
+import { AuthService } from '../auth/auth.service';
 
 @Controller('users')
-export class UserController{
-    constructor(public userService: UserService) { }
+export class UserController {
+    constructor(
+        private authService: AuthService
+    ) { }
 
     @Post()
     createUser(@Body() userInterface: UserInterface) {
-        return this.userService.createUser(userInterface)
+        return this.authService.register(userInterface)
     }
 }
