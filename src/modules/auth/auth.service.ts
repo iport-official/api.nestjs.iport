c156a18a7f0e23bd84aebc311e9fa1a9e3c02c89
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-08-31 22:16:49 -0300

    feat(auth service & local strategy): create the base logic of those files

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService { }
+export class AuthService {
+    constructor(private userService: UserService) { }
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.userService.findOne({ email: username })
+        console.log(user);
+        if (user && user.password == password) {
+            const { email, ...result } = user
+            return result
+        }
+        return null
+    }
+}
--- src/modules/auth/local.strategy.ts
+++ src/modules/auth/local.strategy.ts
@@ -1,16 +1,19 @@
 import { Strategy } from 'passport-local';
-import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
     constructor(private authService: AuthService) {
-        super()
+        super({
+            usernameField: 'email',
+            passwordField: 'password'
+        })
     }
 
-    async validate(email: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(email, password)
+    async validate(username: string, password: string): Promise<any> {
+        const user = await this.authService.validateUser(username, password)
         if (!user) {
             throw new UnauthorizedException()
         }
