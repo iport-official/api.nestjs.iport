73b85a45c211309f9d53ee548997ab994f8a57f2
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-01 18:30:06 -0300

    chore: add @nestjs/config in project

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 
-import { UserModule } from '../user/user.module';
 import { LocalStrategy } from './strategy/local.strategy';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
