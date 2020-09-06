006f314b5fa869532df6b3e8d946bb93d86fe935
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-06 16:57:11 -0300

    refactor(auth.controller.ts): typed some untyped functions

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,54 @@
-import { Controller, Post, Body, Request, UseGuards } from "@nestjs/common";
+import {
+    Controller,
+    Post,
+    Body,
+    Request,
+    UseGuards,
+    UseInterceptors,
+    UploadedFile
+} from "@nestjs/common";
+
+import { FileInterceptor } from '@nestjs/platform-express'
 
 import { AuthService } from "./auth.service";
 import { RegisterPayload } from "./models/register.payload";
 import { LocalAuthGuard } from "../../guards/local/local-auth.guard";
+import { LoginProxy } from "./models/login.proxy";
+import { RegisterProxy } from "./models/register.proxy";
 
 @Controller('users')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    /**
+     * Method that register the user in the database
+    * It is resposible for encrypting the password before send it to the databse
+    * Before return the new created user is changes the password to 'undefined'
+     * @param file stores the profile image of the user
+     * @param registerPayload stores the data that will be used to create the new
+    * user in the database
+     */
     @Post()
-    register(@Body() credentials: RegisterPayload) {
-        return this.authService.register(credentials)
+    @UseInterceptors(FileInterceptor('profileImage', {
+        limits: {
+            fileSize: 2 * 1024 * 1024
+        }
+    }))
+    async register(
+        @UploadedFile() file: any,
+        @Body() registerPayload: RegisterPayload
+    ): Promise<RegisterProxy> {
+        registerPayload.profileImage = file.buffer.toString('base64')
+        return await this.authService.register(registerPayload)
     }
 
+    /**
+     * Method that create a jwt (Json Web Token)
+     * @param credentials stores the data that will be used to crete the jwt
+     */
     @UseGuards(LocalAuthGuard)
     @Post('/login')
-    login(@Request() credentials: any) {
+    async login(@Request() credentials: any): Promise<LoginProxy> {
         return this.authService.login(credentials.user)
     }
 }
