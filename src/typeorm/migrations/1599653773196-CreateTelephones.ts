a840fac350e5543eabc671c06edfb67e5bd46f4e
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-09 09:23:56 -0300

    feat(CreateTelephones & CreateEmails): structured the migrations

--- src/typeorm/migrations/1599653773196-CreateTelephones.ts
+++ src/typeorm/migrations/1599653773196-CreateTelephones.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateTelephones1599653773196 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'telephones',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid'
+                    },
+                    {
+                        name: 'telephoneNumber',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'userId',
+                        type: 'varchar',
+                        isNullable: false
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('telephones')
     }
 
 }
--- src/typeorm/migrations/1599653783631-CreateEmails.ts
+++ src/typeorm/migrations/1599653783631-CreateEmails.ts
@@ -1,11 +1,36 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { isNull } from "util";
 
 export class CreateEmails1599653783631 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'emails',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid'
+                    },
+                    {
+                        name: 'emailAddress',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'userId',
+                        type: 'varchar',
+                        isNullable: false
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('emails')
     }
 
 }
