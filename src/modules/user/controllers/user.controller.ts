fce560e2acd44157de58d2c61fcfc7000262acdb
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-13 20:30:12 -0300

    feat(user.controller.ts & user.service.ts): add the getProfile method, that can get the user data just with the token, passed in bearer

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Post, Body, UseGuards, Get, Request } from '@nestjs/common';
+import { Controller, UseGuards, Get, Request } from '@nestjs/common';
+import { RequestUser } from 'src/decorators/user.decorator';
+import { JwtAuthGuard } from 'src/guards/jwt/jwt-auth.guard';
+import { UserProxy } from '../models/user.proxy';
 
 import { UserService } from '../services/user.service';
 
@@ -8,5 +11,15 @@ export class UserController {
     constructor(
         private readonly userService: UserService
     ) { }
+
+    /**
+     * Method that returns the user based on id
+     * @param user the basic user data, that will be used to get the complete user data
+     */
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    async getProfile(@RequestUser() user: { id: string, username: string }): Promise<UserProxy> {
+        return await this.userService.getProfile(user.id)
+    }
 
 }
