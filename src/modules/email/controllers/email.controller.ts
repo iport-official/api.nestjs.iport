8ffa9c5db4baf34bc23802a2d4070b120b68ccb7
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-23 18:17:24 -0300

    feat(controllers/email.controller.ts): removed the registerEmails method

--- src/modules/email/controllers/email.controller.ts
+++ src/modules/email/controllers/email.controller.ts
@@ -1,27 +1,22 @@
-import { Controller, UseGuards, Post, Body } from "@nestjs/common";
-
-import { JwtAuthGuard } from "src/guards/jwt/jwt-auth.guard";
-import { BaseArrayProxy } from "src/common/base-array-proxy";
-import { EmailPayload } from "../models/email.payload";
-import { EmailProxy } from "../models/email.proxy";
-
-import { EmailService } from "../services/email.service";
+import { Controller } from "@nestjs/common";
 
 @Controller('users/emails')
 export class EmailController {
 
-    constructor(
-        private readonly emailService: EmailService
-    ) { }
-
-    /**
-     * Method that allows creating email and the associating them to users
-     * @param emailPayload indicates the array of emails and the user id
-     */
-    @UseGuards(JwtAuthGuard)
-    @Post()
-    async registerEmails(@Body() emailPayload: EmailPayload): Promise<BaseArrayProxy<EmailProxy>> {
-        return this.emailService.registerEmails(emailPayload);
-    }
+    public constructor() { }
 
 }
+
+//#region Old code
+
+// /**
+//  * Method that allows creating email and the associating them to users
+//  * @param emailPayload indicates the array of emails and the user id
+//  */
+// @UseGuards(JwtAuthGuard)
+// @Post()
+// async registerEmails(@Body() emailPayload: EmailPayload): Promise<BaseArrayProxy<EmailProxy>> {
+//     return this.emailService.registerEmails(emailPayload);
+// }
+
+//#endregion
