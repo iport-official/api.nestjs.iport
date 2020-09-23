49bbaddae5bce2bf96e3e3a0fcc2d3c71e2a01a3
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-23 18:16:09 -0300

    refactor(services/email.service.ts): replaced id parameter and add the user parameter - type UserEntity

--- src/modules/email/services/email.service.ts
+++ src/modules/email/services/email.service.ts
@@ -5,11 +5,10 @@ import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
 
 import { EmailEntity } from "src/typeorm/entities/email.entity";
 import { BaseArrayProxy } from "src/common/base-array-proxy";
-import { EmailPayload } from "../models/email.payload";
 import { EmailProxy } from "../models/email.proxy";
 
-import { UserService } from "src/modules/user/services/user.service";
 import { UserProxy } from "src/modules/user/models/user.proxy";
+import { UserEntity } from "src/typeorm/entities/user.entity";
 
 @Injectable()
 export class EmailService extends TypeOrmCrudService<EmailEntity> {
@@ -17,18 +16,15 @@ export class EmailService extends TypeOrmCrudService<EmailEntity> {
     public constructor(
         @InjectRepository(EmailEntity)
         private readonly repository: Repository<EmailEntity>,
-        @Inject('UserService')
-        private readonly userService: UserService
     ) { super(repository) }
 
     /**
      * Method that allows creating email and the associating them to users
      * @param emailPayload indicates the array of emails and the user id
      */
-    public async registerEmails(emailPayload: EmailPayload): Promise<BaseArrayProxy<EmailProxy>> {
+    public async registerEmails(emails: string[], user: UserEntity): Promise<BaseArrayProxy<EmailProxy>> {
         try {
-            const user = await this.userService.findOne({ where: { id: emailPayload.userId } })
-            const array = await this.repository.save(emailPayload.emails.map(email => {
+            const array = await this.repository.save(emails.map(email => {
                 return {
                     email,
                     user: new UserProxy(user)
@@ -51,9 +47,8 @@ export class EmailService extends TypeOrmCrudService<EmailEntity> {
         }
     }
 
-    public async deleteAllEmailsUsingUserID(userId: string): Promise<void> {
+    public async deleteAllEmailsUsingByUser(user: UserEntity): Promise<void> {
         try {
-            const user = await this.userService.findOne({ where: { id: userId } })
             await this.repository.delete({ user })
         } catch (error) {
             throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
