3538418884eb26ae80ee70ef049e02d62ba84c57
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-09-23 18:15:48 -0300

    refactor(services/telephone.service.ts): replaced id parameter and add the user parameter - type UserEntity

--- src/modules/telephone/services/telephone.service.ts
+++ src/modules/telephone/services/telephone.service.ts
@@ -1,15 +1,14 @@
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
-import { HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
 
 import { TelephoneEntity } from "src/typeorm/entities/telephone.entity";
 import { BaseArrayProxy } from "src/common/base-array-proxy";
 import { TelephoneProxy } from "../models/telephone.proxy";
-import { TelephonePayload } from "../models/telephone.payload";
 
-import { UserService } from "src/modules/user/services/user.service";
 import { UserProxy } from "src/modules/user/models/user.proxy";
+import { UserEntity } from "src/typeorm/entities/user.entity";
 
 @Injectable()
 export class TelephoneService extends TypeOrmCrudService<TelephoneEntity> {
@@ -17,18 +16,15 @@ export class TelephoneService extends TypeOrmCrudService<TelephoneEntity> {
     public constructor(
         @InjectRepository(TelephoneEntity)
         private readonly repository: Repository<TelephoneEntity>,
-        @Inject('UserService') 
-        private readonly userService: UserService
     ) { super(repository) }
 
     /**
      * Method that allows creating telephones and the associating them to users
      * @param telephonePayload indicates the array of telephones and the user id
      */
-    public async registerTelephones(telephonePayload: TelephonePayload): Promise<BaseArrayProxy<TelephoneProxy>> {
+    public async registerTelephones(telephones: string[], user: UserEntity): Promise<BaseArrayProxy<TelephoneProxy>> {
         try {
-            const user = await this.userService.findOne({ where: { id: telephonePayload.userId } })
-            const array = await this.repository.save(telephonePayload.telephones.map(telephone => {
+            const array = await this.repository.save(telephones.map(telephone => {
                 return {
                     telephone,
                     user: new UserProxy(user)
@@ -51,9 +47,8 @@ export class TelephoneService extends TypeOrmCrudService<TelephoneEntity> {
         }
     }
 
-    public async deleteAllTelephonesUsingUserId(userId: string): Promise<void> {
+    public async deleteAllTelephonesByUser(user: UserEntity): Promise<void> {
         try {
-            const user = await this.userService.findOne({ where: { id: userId } })
             await this.repository.delete({ user })
         } catch (error) {
             throw new HttpException('Not Found', HttpStatus.NOT_FOUND)
