5c160c71a3ef9a0c0fda6c65134690115ba2beae
Caique <caiquetorres.bastos@hotmail.com>
2020-09-22 23:56:01 -0300

    feat(update-user.payload.ts): create the file that will store the new user data

---
+++ src/modules/user/models/update-user.payload.ts
@@ -0,0 +1,16 @@
+import { AccountType } from "src/models/enums/account.types"
+
+export class UpdateUserPayload {
+
+    profileImage?: string
+    email?: string
+    password?: string
+    accountType?: AccountType
+    username?: string
+    cpf?: string
+    cnpj?: string
+    cep?: string
+    telephones?: string[]
+    emails?: string[]
+
+}
