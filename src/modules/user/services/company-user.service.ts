462161c86eacbd437324f0329f4308dfa49b2311
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-04 22:34:07 -0300

    feat(services/company-user.service.ts): removed companyUser variables, to make the methods smaller

--- src/modules/user/services/company-user.service.ts
+++ src/modules/user/services/company-user.service.ts
@@ -4,7 +4,6 @@ import { CompanyUserEntity } from '../../../typeorm/entities/company-user.entity
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { RegisterCompanyUserPayload } from '../models/register-company-user.payload';
-import { RegisterCompanyUserProxy } from '../models/register-company-user-proxy';
 
 @Injectable()
 export class CompanyUserService extends TypeOrmCrudService<CompanyUserEntity> {
@@ -16,14 +15,12 @@ export class CompanyUserService extends TypeOrmCrudService<CompanyUserEntity> {
 
     public async registerCompanyAccount(
         registerCompanyAccountPayload: RegisterCompanyUserPayload
-    ): Promise<RegisterCompanyUserProxy> {
-        const companyUser = await this.repository.save(registerCompanyAccountPayload)
-        return new RegisterCompanyUserProxy(companyUser)
+    ): Promise<CompanyUserEntity> {
+        return await this.repository.save(registerCompanyAccountPayload)
     }
 
-    public async getCompanyAccount(id: string) {
-        const companyUsers = await this.repository.find({where:{id}})
-        return new RegisterCompanyUserProxy((companyUsers[0]))
+    public async getCompanyAccount(id: string): Promise<CompanyUserEntity> {
+        return await this.repository.findOne({where:{id}})
     }
 
 }
