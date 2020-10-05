12668f03bbe6c534e5d4ea0657212a16b9dafe5e
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-04 22:34:58 -0300

    feat(services/personal-user.service.ts): removed personalUser variables to make the methods smaller

--- src/modules/user/services/personal-user.service.ts
+++ src/modules/user/services/personal-user.service.ts
@@ -4,7 +4,6 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { PersonalUserEntity } from '../../../typeorm/entities/personal-user.entity'
 import { RegisterPersonalUserPayload } from '../models/register-personal-user.payload'
-import { RegisterPersonalUserProxy } from '../models/register-personal-user.proxy'
 
 @Injectable()
 export class PersonalUserService extends TypeOrmCrudService<PersonalUserEntity> {
@@ -16,14 +15,12 @@ export class PersonalUserService extends TypeOrmCrudService<PersonalUserEntity> 
 
     public async registerPersonalAccount(
         registerPersonalAccountPayload: RegisterPersonalUserPayload
-    ): Promise<RegisterPersonalUserProxy> {
-        const personalUser = await this.repository.save(registerPersonalAccountPayload)
-        return new RegisterPersonalUserProxy(personalUser)
+    ): Promise<PersonalUserEntity> {
+        return await this.repository.save(registerPersonalAccountPayload)
     }
 
-    public async getPersonalAccount(id: string): Promise<RegisterPersonalUserProxy> {
-        const personalUsers = await this.repository.find({ where: { id }})
-        return new RegisterPersonalUserProxy(personalUsers[0])
+    public async getPersonalAccount(id: string): Promise<PersonalUserEntity> {
+        return await this.repository.findOne({ where: { id }})
     }
 
 }
