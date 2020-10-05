6ca8652269a061f68d7c2f953df9c5ff23ef4dda
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-04 22:29:12 -0300

    feat(models/company-user.proxy.ts & personal-user.proxy.ts): create those files

---
+++ src/modules/user/models/personal-user.proxy.ts
@@ -0,0 +1,10 @@
+import { PersonalUserEntity } from '../../../typeorm/entities/personal-user.entity';
+
+export class PersonalUserProxy {
+
+    cpf: string
+
+    public constructor (entity: PersonalUserEntity) {
+        this.cpf = entity.cpf
+    }
+}
