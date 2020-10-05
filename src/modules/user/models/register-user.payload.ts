83b8eaa1911156fb6d31a543c0ab77d41dcccca3
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-04 22:23:58 -0300

    feat(models/register-user.payload.ts): add city, state, content, telephones and emails properties and removed cpf, cnpj and cep properties

--- src/modules/user/models/register-user.payload.ts
+++ src/modules/user/models/register-user.payload.ts
@@ -1,14 +1,18 @@
 import { AccountType } from "src/models/enums/account.types"
+import { RegisterCompanyUserPayload } from './register-company-user.payload';
+import { RegisterPersonalUserPayload } from './register-personal-user.payload';
 
 export class RegisterUserPayload {
 
     profileImage: string
     email: string
     password: string
-    accountType: AccountType
     username: string
-    cpf: string
-    cnpj: string
-    cep: string
+    city: string
+    state: string
+    accountType: AccountType
+    content: RegisterPersonalUserPayload | RegisterCompanyUserPayload
+    telephones: string[]
+    emails: string[]
 
 }
