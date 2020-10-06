fb35d2b47951c5feaa92443eac75e0b46683f545
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-05 21:33:55 -0300

    feat(models/register.proxy.ts): removed unused file

--- src/modules/auth/models/register.proxy.ts
+++
@@ -1,8 +0,0 @@
-import { UserProxy } from 'src/modules/user/models/user.proxy'
-import { UserEntity } from 'src/typeorm/entities/user.entity'
-
-export class RegisterProxy extends UserProxy {
-    public constructor(entity: UserEntity) {
-        super(entity)
-    }
-}
