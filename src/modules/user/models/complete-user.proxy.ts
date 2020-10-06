206d9a68a2e5d0f3bfd0e67c6c18d8dd1529ca6c
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-06 15:56:51 -0300

    fix(complete-user.proxy.ts & simple-user.proxy.ts): renamed the user.proxy.ts and create a new proxy named base-user.proxy.ts, that allows returning just the data that the dev want

--- src/modules/user/models/complete-user.proxy.ts
+++ src/modules/user/models/complete-user.proxy.ts
@@ -3,27 +3,15 @@ import { UserEntity } from 'src/typeorm/entities/user.entity'
 import { PersonalUserProxy } from './personal-user.proxy'
 import { CompanyUserProxy } from './company-user.proxy'
 import { BaseArrayProxy } from 'src/common/base-array-proxy'
+import { BasicUserProxy } from './simple-user.proxy'
 
-export class UserProxy {
-    id: string
-    email: string
-    username: string
-    accountType: AccountType
-    createAt: Date
-    updateAt: Date
+export class CompleteUserProxy extends BasicUserProxy {
     content: PersonalUserProxy | CompanyUserProxy
     telephones: BaseArrayProxy<string>
     emails: BaseArrayProxy<string>
-    profileImage: string
 
     public constructor(entity: UserEntity) {
-        this.id = entity.id
-        this.email = entity.email
-        this.username = entity.username
-        this.accountType = entity.accountType
-        this.createAt = entity.createAt
-        this.updateAt = entity.updateAt
-        this.profileImage = entity.profileImage
+        super(entity)
 
         this.content =
             entity.accountType === AccountType.PERSONAL
