ec3b7b54934c7a0797574459dfd1efa3febb5fbe
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-09 18:29:55 -0300

    feat(services/competence.service.ts): create the main methods in competence service (getCompetences, getCompetencesById, createCompetence)

--- src/modules/competences/services/competence.service.ts
+++ src/modules/competences/services/competence.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
+import { Repository } from 'typeorm'
 
-import { CompetenceEntity } from "src/typeorm/entities/competence.entity";
+import { CompetenceEntity } from 'src/typeorm/entities/competence.entity'
+import { UserEntity } from 'src/typeorm/entities/user.entity'
 
-import { UserService } from "src/modules/user/services/user.service";
+import { CreateCompetencePayload } from '../models/create-competence.payload'
+
+import { UserService } from 'src/modules/user/services/user.service'
 
 @Injectable()
 export class CompetenceService extends TypeOrmCrudService<CompetenceEntity> {
@@ -15,5 +18,41 @@ export class CompetenceService extends TypeOrmCrudService<CompetenceEntity> {
         private readonly userService: UserService
     ) {
         super(repository)
+    }
+
+    public async createCompetence(
+        userId: string,
+        createCompetencePayload: CreateCompetencePayload
+    ): Promise<CompetenceEntity> {
+        const user = await this.userService.getUserById(userId)
+        return await this.repository.save({
+            ...createCompetencePayload,
+            user
+        })
+    }
+
+    public async getCompetencesById(id: string): Promise<CompetenceEntity> {
+        const competence = await this.repository
+            .createQueryBuilder('competences')
+            .where({ id })
+            .innerJoinAndSelect('competences.user', 'users.id')
+            .getOne()
+        const user = await this.userService.getProfile(competence.user)
+        competence.user = user
+        return competence
+    }
+
+    public async getCompetences(
+        userId: string
+    ): Promise<{
+        user: UserEntity
+        competences: CompetenceEntity[]
+    }> {
+        const user = await this.userService.getUserById(userId)
+        const competences = await this.repository.find({ where: { user } })
+        return {
+            user,
+            competences
+        }
     }
 }
