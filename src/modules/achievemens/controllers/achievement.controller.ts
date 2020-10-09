71fe4353fa45a6c392763de95a4093974bd9d365
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-09 16:41:51 -0300

    feat(controllers/achievement.controller.ts & services/achievement.service.ts): create the main logic of those files

--- src/modules/achievemens/controllers/achievement.controller.ts
+++ src/modules/achievemens/controllers/achievement.controller.ts
@@ -1,4 +1,77 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common'
+
+import { AchievementProxy } from '../models/achievement.proxy'
+import { CreateAchievementPayload } from '../models/create-achievement.payload'
+import { ArrayProxy } from 'src/common/array-proxy'
+import { BasicUserProxy } from 'src/modules/user/models/simple-user.proxy'
+
+import { AchievementService } from '../services/achievements.service'
+
+import { RequestUserProperties } from 'src/common/jwt-validation-properties'
+import { RequestUser } from 'src/decorators/user.decorator'
+import { JwtAuthGuard } from 'src/guards/jwt/jwt-auth.guard'
 
 @Controller()
-export class AchievementController {}
+export class AchievementController {
+    public constructor(
+        private readonly achievementService: AchievementService
+    ) {}
+
+    /**
+     * Method that can create a new achievement entity in the database
+     * @param requestUser stores the user basic data (id, email, accountType)
+     * @param createAchievementPayload stores the new achievement data
+     */
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    public async createAchievement(
+        @RequestUser() requestUser: RequestUserProperties,
+        @Body() createAchievementPayload: CreateAchievementPayload
+    ): Promise<AchievementProxy> {
+        const achievement = await this.achievementService.createAchievement(
+            requestUser.id,
+            createAchievementPayload
+        )
+        return new AchievementProxy(achievement)
+    }
+
+    /**
+     * Method that can get a specific achievement entity
+     * @param id stores the achievement id
+     */
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    public async getAchievementById(
+        @Param('id') id: string
+    ): Promise<AchievementProxy> {
+        const achievement = await this.achievementService.getAchievementById(id)
+        return new AchievementProxy(achievement)
+    }
+
+    /**
+     * Method that can get all achievements using the user id
+     * @param userId stores the user id
+     */
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    public async getAchievements(
+        @Param('userId') userId: string
+    ): Promise<{
+        user: BasicUserProxy
+        achievements: ArrayProxy<AchievementProxy>
+    }> {
+        const {
+            user,
+            achievements
+        } = await this.achievementService.getAchievements(userId)
+        return {
+            user: new BasicUserProxy(user),
+            achievements: {
+                length: achievements.length,
+                array: achievements.map(
+                    achievement => new AchievementProxy(achievement)
+                )
+            }
+        }
+    }
+}
--- src/modules/achievemens/services/achievements.service.ts
+++ src/modules/achievemens/services/achievements.service.ts
@@ -1,4 +1,71 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
+import { Repository } from 'typeorm'
+
+import { AchievementEntity } from 'src/typeorm/entities/achievement.entity'
+import { UserEntity } from 'src/typeorm/entities/user.entity'
+
+import { CreateAchievementPayload } from '../models/create-achievement.payload'
+
+import { UserService } from 'src/modules/user/services/user.service'
 
 @Injectable()
-export class AchievementService {}
+export class AchievementService extends TypeOrmCrudService<AchievementEntity> {
+    public constructor(
+        @InjectRepository(AchievementEntity)
+        private readonly repository: Repository<AchievementEntity>,
+        private readonly userService: UserService
+    ) {
+        super(repository)
+    }
+
+    /**
+     * Method that can create a new achievement entity in the database
+     * @param userId stores the user id
+     * @param createAchievementPayload stores the new achievement data
+     */
+    public async createAchievement(
+        userId: string,
+        createAchievementPayload: CreateAchievementPayload
+    ): Promise<AchievementEntity> {
+        const user = await this.userService.getUserById(userId)
+        return await this.repository.save({
+            ...createAchievementPayload,
+            user
+        })
+    }
+
+    /**
+     * Method that can get a specific achievement entity
+     * @param id stores the achievement id
+     */
+    public async getAchievementById(id: string): Promise<AchievementEntity> {
+        const achievement = await this.repository
+            .createQueryBuilder('achievements')
+            .where({ id })
+            .innerJoinAndSelect('achievements.user', 'users.id')
+            .getOne()
+        const user = await this.userService.getProfile(achievement.user)
+        achievement.user = user
+        return achievement
+    }
+
+    /**
+     * Method that can get all achievements using the user id
+     * @param userId stores the user id
+     */
+    public async getAchievements(
+        userId: string
+    ): Promise<{
+        user: UserEntity
+        achievements: AchievementEntity[]
+    }> {
+        const user = await this.userService.getUserById(userId)
+        const achievements = await this.repository.find({ where: { user } })
+        return {
+            user,
+            achievements
+        }
+    }
+}
