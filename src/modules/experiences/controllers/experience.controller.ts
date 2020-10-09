8910215b5b01e3d65b52f012c71af018d57c9f22
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-09 11:37:40 -0300

    feat(controllers/experience.controller.ts & services/experience.service.ts): create getting and creating logics in those files

--- src/modules/experiences/controllers/experience.controller.ts
+++ src/modules/experiences/controllers/experience.controller.ts
@@ -1,8 +1,64 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common'
+
+import { BasicExperienceProxy } from '../models/basic-experience.proxy'
+import { CompleteExperienceProxy } from '../models/complete-experience.proxy'
+import { CreateExperiencePayload } from '../models/create-experience.payload'
+import { CreateExperienceProxy } from '../models/create-experience.proxy'
+import { BaseArrayProxy } from 'src/common/base-array-proxy'
+import { BasicUserProxy } from 'src/modules/user/models/simple-user.proxy'
 
 import { ExperienceService } from '../services/experience.service'
 
-@Controller()
+import { RequestUserProperties } from 'src/common/jwt-validation-properties'
+import { RequestUser } from 'src/decorators/user.decorator'
+import { JwtAuthGuard } from 'src/guards/jwt/jwt-auth.guard'
+
+@Controller('users/:userId/experiences')
 export class ExperienceController {
     public constructor(private readonly experienceService: ExperienceService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    public async createExperience(
+        @RequestUser() requestUser: RequestUserProperties,
+        @Body() createExperiencePayload: CreateExperiencePayload
+    ): Promise<CreateExperienceProxy> {
+        const experience = await this.experienceService.createExperience(
+            requestUser.id,
+            createExperiencePayload
+        )
+        return new CreateExperienceProxy(experience)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get(':id')
+    public async getExperienceById(
+        @Param('id') id: string
+    ): Promise<CompleteExperienceProxy> {
+        const experience = await this.experienceService.getExperienceById(id)
+        return new CompleteExperienceProxy(experience)
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    public async getExperiences(
+        @Param('userId') userId: string
+    ): Promise<{
+        user: BasicUserProxy
+        experiences: BaseArrayProxy<BasicExperienceProxy>
+    }> {
+        const {
+            user,
+            experiences
+        } = await this.experienceService.getExperiences(userId)
+        return {
+            user: new BasicUserProxy(user),
+            experiences: {
+                length: experiences.length,
+                array: experiences.map(
+                    experience => new BasicExperienceProxy(experience)
+                )
+            }
+        }
+    }
 }
--- src/modules/experiences/services/experience.service.ts
+++ src/modules/experiences/services/experience.service.ts
@@ -4,6 +4,9 @@ import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
 import { Repository } from 'typeorm'
 
 import { ExperienceEntity } from 'src/typeorm/entities/experience.entity'
+import { UserEntity } from 'src/typeorm/entities/user.entity'
+
+import { CreateExperiencePayload } from '../models/create-experience.payload'
 
 import { UserService } from 'src/modules/user/services/user.service'
 
@@ -15,5 +18,41 @@ export class ExperienceService extends TypeOrmCrudService<ExperienceEntity> {
         private readonly userService: UserService
     ) {
         super(repository)
+    }
+
+    public async createExperience(
+        id: string,
+        createExperiencePayload: CreateExperiencePayload
+    ): Promise<ExperienceEntity> {
+        const user = await this.userService.getUserById(id)
+        return await this.repository.save({
+            ...createExperiencePayload,
+            user
+        })
+    }
+
+    public async getExperienceById(id: string): Promise<ExperienceEntity> {
+        const experience = await this.repository
+            .createQueryBuilder('experiences')
+            .where({ id })
+            .innerJoinAndSelect('experiences.user', 'users.id')
+            .getOne()
+        const user = await this.userService.getProfile(experience.user)
+        experience.user = user
+        return experience
+    }
+
+    public async getExperiences(
+        id: string
+    ): Promise<{
+        user: UserEntity
+        experiences: ExperienceEntity[]
+    }> {
+        const user = await this.userService.getUserById(id)
+        const experiences = await this.repository.find({ where: { user } })
+        return {
+            user,
+            experiences
+        }
     }
 }
