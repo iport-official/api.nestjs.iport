a03922f8b6701a96714fad231c6e7d0abc4b8d15
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-13 20:57:05 -0300

    feat(email/email.module.ts): add UserModule in EmailModule imports

--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { EmailEntity } from 'src/typeorm/entities/email.entity'
 
+import { EmailController } from './controllers/email.controller'
+
 import { EmailService } from './services/email.service'
 
+import { UserModule } from '../user/user.module'
+
 @Module({
-    imports: [TypeOrmModule.forFeature([EmailEntity])],
+    imports: [UserModule, TypeOrmModule.forFeature([EmailEntity])],
     providers: [EmailService],
+    controllers: [EmailController],
     exports: [EmailService]
 })
 export class EmailModule {}
