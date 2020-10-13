7dc0bc38cbc18177977590c8ee64ffd035b11f33
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-13 20:55:15 -0300

    feat(services/email.service.ts): removed some unused methods and created another ones

--- src/modules/email/services/email.service.ts
+++ src/modules/email/services/email.service.ts
@@ -1,86 +1,74 @@
-import {
-    Injectable,
-    InternalServerErrorException,
-    NotFoundException
-} from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm'
 import { Repository } from 'typeorm'
 
 import { EmailEntity } from 'src/typeorm/entities/email.entity'
-import { UserEntity } from 'src/typeorm/entities/user.entity'
+
+import { UserService } from 'src/modules/user/services/user.service'
 
 @Injectable()
 export class EmailService extends TypeOrmCrudService<EmailEntity> {
     public constructor(
         @InjectRepository(EmailEntity)
-        private readonly repository: Repository<EmailEntity>
+        private readonly repository: Repository<EmailEntity>,
+        private readonly userService: UserService
     ) {
         super(repository)
     }
 
     /**
-     * Method that allows creating email and the associating them to users
-     * @param emailPayload indicates the array of emails and the user id
+     * Method that allow creating emails and the associating them to users
+     * @param emails stores an array with all the user emails
+     * @param userId stores the user id
      */
     public async registerEmails(
         emails: string[],
-        user: UserEntity
+        userId: string
     ): Promise<EmailEntity[]> {
-        try {
-            if (!emails || emails.length === 0 || user === null) return null
-            return await this.repository.save(
-                emails.map(email => {
-                    return {
-                        email,
-                        user
-                    }
-                })
-            )
-        } catch (error) {
-            throw new InternalServerErrorException(error)
-        }
+        const user = await this.userService.getUserById(userId)
+        if (!user) throw new NotFoundException('User not found')
+        return await this.repository.save(
+            emails.map(email => {
+                return {
+                    email,
+                    user
+                }
+            })
+        )
     }
 
+    /**
+     * Method that can update all the user's emails
+     * @param emails stores an array of strings representing the emails
+     * @param userId stores the user id
+     */
     public async updateEmails(
         emails: string[],
-        user: UserEntity
-    ): Promise<void> {
-        await this.deleteAllEmailsUsingByUser(user)
-        await this.registerEmails(emails, user)
+        userId: string
+    ): Promise<EmailEntity[]> {
+        const user = await this.userService.getUserById(userId)
+        if (!user) throw new NotFoundException('User not found')
+        await this.repository.delete({ user })
+        return await this.repository.save(
+            emails.map(email => {
+                return {
+                    email,
+                    user
+                }
+            })
+        )
     }
 
     /**
      * Method that can get the user's emails array
-     * @param user stores the entity that will be used to find the user's emails array
+     * @param userId stores the user id
      */
-    public async getEmailsFromUser(user: UserEntity): Promise<EmailEntity[]> {
+    public async getEmailsFromUser(userId: string): Promise<EmailEntity[]> {
+        const user = await this.userService.getUserById(userId)
+        if (!user) throw new NotFoundException('User not found')
         const emails = await this.repository.find({ user })
         if (!emails) throw new NotFoundException('Emails not found')
         return emails
-    }
-
-    /**
-     * Method that can delete a specific email
-     * @param id indicates the unique id that this email has
-     */
-    public async deleteEmail(id: string): Promise<void> {
-        try {
-            await this.repository.delete({ id })
-        } catch (error) {
-            throw new NotFoundException(error)
-        }
-    }
-
-    /**
-     * Method that can delete all the user's emails
-     * @param user indicates the user that will have all the emails deleted
-     */
-    public async deleteAllEmailsUsingByUser(user: UserEntity): Promise<void> {
-        try {
-            await this.repository.delete({ user })
-        } catch (error) {
-            throw new NotFoundException(error)
-        }
     }
 }
