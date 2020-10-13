e33df0094347405088c5ea46f627f5b44d57b67d
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-13 15:33:19 -0300

    feat(models/update-user.payload.ts): add some missing properties

--- src/modules/user/models/update-user.payload.ts
+++ src/modules/user/models/update-user.payload.ts
@@ -1,14 +1,17 @@
 import { AccountType } from 'src/models/enums/account.types'
 
+import { RegisterCompanyUserPayload } from './register-company-user.payload'
+import { RegisterPersonalUserPayload } from './register-personal-user.payload'
+
 export class UpdateUserPayload {
     profileImage?: string
     email?: string
     password?: string
-    accountType?: AccountType
     username?: string
-    cpf?: string
-    cnpj?: string
-    cep?: string
+    city?: string
+    state?: string
+    accountType?: AccountType
+    content?: RegisterPersonalUserPayload | RegisterCompanyUserPayload
     telephones?: string[]
     emails?: string[]
 }
