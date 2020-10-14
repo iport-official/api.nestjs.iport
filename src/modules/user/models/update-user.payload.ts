eb7d3a102ecb663e84ed62a1ab176d083e7788a1
CaiqueTorres <caiquetorres.bastos@hotmail.com>
2020-10-13 23:38:10 -0300

    feat(models/update-user.payload.ts): removed some unecessary properties

--- src/modules/user/models/update-user.payload.ts
+++ src/modules/user/models/update-user.payload.ts
@@ -1,17 +1,11 @@
-import { AccountType } from 'src/models/enums/account.types'
-
 import { UpdateCompanyUserPayload } from './update-company-user.payload'
 import { UpdatePersonalUserPayload } from './update-personal-user.payload'
 
 export class UpdateUserPayload {
     profileImage?: string
     email?: string
-    password?: string
     username?: string
     city?: string
     state?: string
-    accountType?: AccountType
     content?: UpdatePersonalUserPayload | UpdateCompanyUserPayload
-    telephones?: string[]
-    emails?: string[]
 }
